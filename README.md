93b034612017802dc4ea75d59b2314cbdb6ffee5
Chris (Someguy123) <info@someguy123.com>
2020-12-12 23:32:15 +0000

    v1.0.0 - Initial release of Privex's usbreset fork

--- README.md
+++ README.md
@@ -1,50 +1,112 @@
 # Linux USB Device Reset Application
 
-This application can be used to perform resets on USB devices.  This README describes the steps to compile, run, and then automate execution of this application to reset a USB wireless adapter with a greater degree of authority than is available using `ifdown wlan0`/`ifup wlan0`.
+This application can be used to reset USB devices on Linux, saving you from having to unplug and re-plug the device back in.
+
+This is a fork of the original version, uploaded by @jkulesza https://github.com/jkulesza/usbreset - which itself was originally
+written by Alan Stern. 
+
+We've (Privex Inc.) made various improvements on the original version, such as:
+
+ - Adding detailed help and usage information displayed when you run the program without arguments
+ - Improved the error messages to be more descriptive, to help users understand why it went wrong.
+ - A completely new feature (part of the help/usage) which loops over the available USB devices and 
+   outputs their paths, allowing a user to easily copy paste the direct device path.
+
+This tool is especially useful in scenarios where you don't have physical access to the system, for example, a co-located server.
+
+If the device is still recognised by Linux (shows up in `lsusb`), but is malfunctioning, then you can often reset just that
+individual device.
+
+If the device isn't recognised by Linux due to severe malfunctioning, and doesn't show up in `lsusb`, you can still reset
+the device by resetting the USB port / hub that the device is connected to, which will force Linux to re-detect the device
+and try to bring it online again.
+
+
+## Quickstart
+
+If you'd rather skip the detailed step-by-step example, simply follow these quickstart commands :)
+
+It's best to run these under the `root` user, or if you prefer, you can simply use `sudo` when running `./usbreset` .
+
+```sh
+# Use lsusb and/or dmesg to find the appropriate device + bus you need to reset
+lsusb
+
+# Download usbreset.c
+wget https://raw.githubusercontent.com/Privex/usbreset/master/usbreset.c
+
+# Compile usbreset.c using either 'cc' or 'gcc'
+cc usbreset.c -o usbreset
+
+# Assuming the problematic device is Bus 003 Device 002
+# you'd pass the following device path
+./usbreset /dev/bus/usb/003/002
+
+# You can also run usbreset with no arguments to see help / usage information,
+# plus a list of available USB device paths that can be used.
+# If you can't reset the device itself (e.g. because the device isn't recognised),
+# you can reset the port / hub device it's connected to instead.
+./usbreset
+```
+
+## License
+
+This fork by Privex, is released under the MIT / X11 open source License.
+
+## Step-by-step example
+
+This README describes the steps to compile, run, and then automate execution of this application to reset a USB wireless adapter with a greater degree of authority than is available using `ifdown wlan0`/`ifup wlan0`.
 
 Credit to Alan Stern for developing this program, as retrieved from: http://marc.info/?l=linux-usb&m=121459435621262&w=2
 
 The following sequence is adequate to execute the program:
 
+
 1. Compile the program:
 
-   `cc usbreset.c -o usbreset`
+
+        cc usbreset.c -o usbreset
+
 
 2. Set the permissions on the executable:
 
-   `chmod +x usbreset`
+
+        chmod +x usbreset
+
 
 3. Get the Bus and Device ID of the USB device you want to reset:
 
-   ```
-   lsusb
-   ```
+   
+        lsusb
+
+
    which provides output:
 
-   ```
-   Bus 001 Device 007: ID 050d:945a Belkin Components F7D1101 v1 Basic Wireless Adapter [Realtek RTL8188SU]
-   Bus 001 Device 003: ID 0403:6014 Future Technology Devices International, Ltd FT232H Single HS USB-UART/FIFO IC
-   Bus 001 Device 002: ID 0bc2:5031 Seagate RSS LLC FreeAgent GoFlex USB 3.0
-   Bus 001 Device 001: ID 1d6b:0002 Linux Foundation 2.0 root hub
-   Bus 002 Device 002: ID 046d:c06b Logitech, Inc. G700 Wireless Gaming Mouse
-   Bus 002 Device 003: ID 413c:2106 Dell Computer Corp. Dell QuietKey Keyboard
-   Bus 002 Device 001: ID 1d6b:0001 Linux Foundation 1.1 root hub
-   ```
+   
+        Bus 001 Device 007: ID 050d:945a Belkin Components F7D1101 v1 Basic Wireless Adapter [Realtek RTL8188SU]
+        Bus 001 Device 003: ID 0403:6014 Future Technology Devices International, Ltd FT232H Single HS USB-UART/FIFO IC
+        Bus 001 Device 002: ID 0bc2:5031 Seagate RSS LLC FreeAgent GoFlex USB 3.0
+        Bus 001 Device 001: ID 1d6b:0002 Linux Foundation 2.0 root hub
+        Bus 002 Device 002: ID 046d:c06b Logitech, Inc. G700 Wireless Gaming Mouse
+        Bus 002 Device 003: ID 413c:2106 Dell Computer Corp. Dell QuietKey Keyboard
+        Bus 002 Device 001: ID 1d6b:0001 Linux Foundation 1.1 root hub
+
+
    where the device of interest is on Bus 001 identified as Device 007 (our USB wireless adapter).
 
 
 4. Execute the program with sudo privilege or as the root user, making necessary substitution for with the `Bus` and `Device` ids as found by running the lsusb command:
 
-   ```sudo ./usbreset /dev/bus/usb/Bus/Device```
+       sudo ./usbreset /dev/bus/usb/Bus/Device
 
    or in our case
 
-   ```sudo ./usbreset /dev/bus/usb/001/007```
+       sudo ./usbreset /dev/bus/usb/001/007
 
    Now, we can automate execution with the following inline Perl command (assuming that `usbreset` is in the $PATH):
 
 5. Execute or embed in a script:
 
-    ```echo $(lsusb | grep "Wireless Adapter");  wifipath=$( lsusb | grep "Wireless Adapter" | perl -nE "/\D+(\d+)\D+(\d+).+/; print qq(\$1/\$2)")  sudo usbreset /dev/bus/usb/$wifipath```
+        echo $(lsusb | grep "Wireless Adapter");  wifipath=$( lsusb | grep "Wireless Adapter" | perl -nE "/\D+(\d+)\D+(\d+).+/; print qq(\$1/\$2)")  sudo usbreset /dev/bus/usb/$wifipath
 
     which was inspired by the code provided by *knb* at http://askubuntu.com/questions/645/how-do-you-reset-a-usb-device-from-the-command-line
--- usbreset.c
+++ usbreset.c
@@ -1,39 +1,194 @@
-/* usbreset -- send a USB port reset to a USB device */
-
+/*
+ * (C) USBReset - Copyright 2020 - Privex Inc. ( https://www.privex.io )
+ *     Released under the MIT / X11 License
+ *     Based on the original usbreset.c written by Alan Stern
+ *     Official repo: https://github.com/Privex/usbreset
+ *
+ * Usage:
+ *
+ *   gcc usbreset.c -o usbreset
+ *   # To view help / usage info, plus a list of available USB device paths:
+ *   ./usbreset
+ *   # To reset device 002 on bus 003 (run lsusb to find the device/port you want to reset)
+ *   ./usbreset /dev/bus/usb/003/002
+ *
+ *   # On some systems, you may want to use 'cc' instead of 'gcc'
+ *   cc usbreset.c -o usbreset
+ *
+ */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <dirent.h>
 #include <sys/ioctl.h>
 
 #include <linux/usbdevice_fs.h>
+#define USBDIR "/dev/bus/usb"
+#define APPVER "1.0.0"
 
+int getfcount(char* dpath) {
+    DIR *d;
+    int totalf = 0;
+    struct dirent *dir;
+    d = opendir(dpath);
+    if (d) {
+        while ((dir = readdir(d)) != NULL) {
+            totalf = totalf + 1;
+        }
+        closedir(d);
+    }
+    return totalf;
+}
+
+void lfiles(char* dpath, char **result) {
+    /*
+    Scans the folder 'dpath' for files and folders, and injects them into the 
+    character array pointer 'result'
+
+
+    Example usage:
+
+        // First use getfcount to figure out how many files are in a certain folder
+        int totalfiles = getfcount("/home/example");
+        // Create a character array pointer using the size returned by getfcount
+        char *flist[totalfiles];
+        // Pass the folder path to lfiles along with the char array pointer to fill
+        lfiles("/home/example", flist);
+        // Now you can loop over the files as needed.
+        for (int i=0;i < totalfiles;i++) {
+            printf("File: %s \n", flist[i]);
+        }
+
+    */
+
+    DIR *d;
+    int totalf = 0;
+    struct dirent *dir;
+
+    // printf("Opening folder: %s \n", dpath);
+    d = opendir(dpath);
+    if (d) {
+        // printf("Folder exists: %s \n", dpath);
+        while ((dir = readdir(d)) != NULL) {
+            // printf("Adding folder: %s \n", dir->d_name);
+            result[totalf] = malloc(strlen(dir->d_name) + 1);
+            strcpy(result[totalf], dir->d_name);
+            totalf = totalf + 1;
+        }
+        // printf("Closing: %s \n", dpath);
+        closedir(d);
+    } else {
+        // printf("d is not valid\n");
+        return;
+    }
+}
+
+void print_copyright() {
+    const char* copyright = 
+        "(C) USBReset Version v%s - Copyright 2020 - Privex Inc. ( https://www.privex.io )\n"
+        "    Released under the MIT / X11 License\n"
+        "    Based on the original usbreset.c written by Alan Stern\n"
+        "    Official repo: https://github.com/Privex/usbreset\n\n";
+
+    fprintf(stderr, copyright, APPVER);
+}
+
+void print_descr() {
+    const char* descr = 
+        "This program resets a given USB device, which can either be an actual connected USB device,\n"
+        "or a USB hub / port (as long as they have a USB device file to use).\n"
+        "\n"
+        "To figure out which bus / port a certain device is connected to, you can either use the\n"
+        "utility 'lsusb' (package 'usbutils' on ubuntu/debian), or check 'dmesg' if the device is\n"
+        "malfunctioning and not showing up in 'lsusb'.\n"
+        "\n"
+        "USB device files are generally found under the devfs location /dev/bus/usb - which usually contains\n"
+        "both connected devices, and the hubs/ports themselves, laid out as /dev/bus/usb/<bus>/<device> \n\n";
+    fprintf(stderr, descr);
+}
+
+void print_usage(char* app_exe) {
+    const char* usagex = 
+        "Usage Example:\n\n"
+        "    To reset 'Device 001' on 'Bus 001', you would run %s like so:\n\n"
+        "        %s /dev/bus/usb/001/001\n\n"
+        "    To reset 'Device 002' on 'Bus 003', you would run %s like so:\n\n"
+        "        %s /dev/bus/usb/003/002\n\n";
+
+    fprintf(stderr, usagex, app_exe, app_exe, app_exe, app_exe);
+}
+
+void print_usbs() {
+    fprintf(stderr, "Available USB devices / buses:\n\n");
+
+    int totalfiles = getfcount(USBDIR);
+    int usblen = strlen(USBDIR);
+    char *flist[totalfiles];  // Create a char array that can store up to 'totalfiles' filenames
+    lfiles(USBDIR, flist);  // Retrieve a list of files into flist
+    //int subf_total;
+
+    for (int i=0;i < totalfiles;i++) {
+        if (strcmp(flist[i], ".") == 0) continue;
+        if (strcmp(flist[i], "..") == 0) continue;
+        fprintf(stderr, "    Bus %s:\n\n", flist[i]);
+        {
+            int sflen = strlen(flist[i]);
+
+            char sfile[sflen + usblen + 1];
+            strcpy(sfile, USBDIR);    // first initialise sfile by copying USBDIR into it
+            strcat(sfile, "/");
+            strcat(sfile, flist[i]);  // append the filename to the end
+            // just like before, retrieve a list of all files in this sub-folder
+            int sftotal = getfcount(sfile);
+            char *sflist[sftotal];
+            lfiles(sfile, sflist);
+            for (int x=0;x < sftotal;x++) {
+                if (strcmp(sflist[x], ".") == 0) continue;
+                if (strcmp(sflist[x], "..") == 0) continue;
+                fprintf(stderr, "        %s/%s\n", sfile, sflist[x]);
+            }
+            fprintf(stderr, "\n");
+        }
+    }
+    fprintf(stderr, "\n");
+}
+
+
+void print_help(char* app_exe) {
+    fprintf(stderr, "Usage: %s device-filename\n\n", app_exe);
+    print_copyright();
+    print_descr();
+    print_usage(app_exe);
+    print_usbs();
+}
 
 int main(int argc, char **argv)
 {
     const char *filename;
-    int fd;
-    int rc;
-
     if (argc != 2) {
-        fprintf(stderr, "Usage: usbreset device-filename\n");
+        print_help(argv[0]);
         return 1;
     }
+    int fd;
+    int rc;
     filename = argv[1];
-
+    printf("Opening handle for USB device %s\n", filename);
     fd = open(filename, O_WRONLY);
     if (fd < 0) {
-        perror("Error opening output file");
+        perror("Error opening device file...");
         return 1;
     }
 
     printf("Resetting USB device %s\n", filename);
     rc = ioctl(fd, USBDEVFS_RESET, 0);
     if (rc < 0) {
-        perror("Error in ioctl");
+        perror("Error in ioctl (error while sending USBDEVFS_RESET control signal to device file)");
         return 1;
     }
-    printf("Reset successful\n");
+    printf("Successfully reset USB device %s  :)\n", filename);
 
     close(fd);
     return 0;
